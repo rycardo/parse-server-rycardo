f35001f302ba0a47745ef94ea9a8cd9d80e22a21
Rycardo <rycardo.cousteau@yahoo.ca>
2017-03-18 16:14:37 -0400

    New Method

--- cloud/main.js
+++ cloud/main.js
@@ -62,6 +62,19 @@ var twilioURL               = process.env.TWILIO_URL            || "127.0.0.1";
 var twilioMount             = process.env.TWILIO_MOUNT          || "/";
 var twilioSendingNumber     = process.env.TWILIO_PHONE_NUMBER;
 
+///////////////////////////////////////
+// CONSTANTS
+//////////////////////////////////////
+
+// User Actions - for verifying a User
+const ACTION_USER_ERROR         = 0;
+const ACTION_USER_CREATE        = 1;
+const ACTION_USER_LOGIN         = 2;
+const ACTION_USER_CONVERT       = 4;
+const ACTION_USER_VERIFY        = 8;
+const ACTION_USER_ACTIVE        = 16;
+const ACTION_USER_LOGGED_IN     = 32;
+
 //////////////////////////////////////
 //
 // hello
@@ -180,6 +193,314 @@ Parse.Cloud.define("barberIdForBarberName", function(request, response)
             console.log("barber name look up failed");
             console.log(error);
             response.error("barber name lookup failed: " + error);
+        }
+    });
+});
+
+
+///////////////////////////////////////
+//
+// determineHowToHandleUserWith
+//
+// Params:
+// emailAddress
+// phoneNumber
+// verificationCode
+// firstName
+// lastName
+//
+// Success Response:
+//  ACTION_ERROR    0
+//  ACTION_CREATE   1
+//  ACTION_LOGIN    2
+//  ACTION_CONVERT  4
+//  ACTION_VERIFY   8
+//  ACTION_USER     16
+
+///////////////////////////////////////
+Parse.Cloud.define("determineHowToHandleUserWith", function(request, response)
+{
+/*
+                                    EmailAddress:(NSString *)emailAddress
+									 phoneNumber:(NSString *)phoneNumber
+								verificationCode:(NSString *)verificationCode
+									   firstName:(NSString *)firstName
+										lastName:(NSString *)lastName
+*/
+    if ( ( request.params.emailAddress === undefined ) ||
+         ( request.params.phoneNumber === undefined  ) ||
+         ( request.params.firstName === undefined    ) ||
+         ( request.params.lastName === undefined     ) )
+    {
+        response.error("Missing Required Parameters.");
+    }
+
+    var verificationCode    = "";
+    if ( request.params.verificationCode.length > 0 )
+    {
+        verificationCode    = request.params.verificationCode;
+        verificationCode    = verificationCode.replace(/\D/g, "");
+    }
+
+    var pmPhoneNumber       = request.params.phoneNumber;
+    pmPhoneNumber           = pmPhoneNumber.replace(/\D/g, "");
+
+    var pmEmailAddress      = request.params.emailAddress;
+    var pmFirstName         = request.params.firstName;
+    var pmLastName          = request.params.lastName;
+
+    var theResult           = {};
+
+    if ( request.user )
+    {
+        // Current User Passed
+        conditionalLog("Current User Passed");
+
+        var cuFirstName     = request.user.get("firstName");
+        var cuLastName      = request.user.get("lastName");
+
+        // Current User Exists, compare information
+        if ( request.user.username === pmEmailAddress )
+        {
+            conditionalLog("Username is email address");
+
+            if ( ( cuFirstName === pmFirstName ) &&
+                 ( cuLastName === pmLastName ) )
+            {
+                // First and Last Names match
+                conditionalLog("First and Last names match");
+                conditionalLog("Convert User to Version 2");
+
+                theResult   = {
+                                action : ( ACTION_USER_CONVERT ),
+                                description: "Convert user to version 2"
+                              };
+            }
+            else
+            {
+                // First and Last Names don't match
+                conditionalLog("First and Last names don't match");
+                conditionalLog("Convert User after Verifying");
+
+                theResult   = {
+                                action : ( ACTION_USER_CONVERT | ACTION_USER_VERIFY ),
+                                description: "Convert user to version 1; Verify user"
+                              };
+            }
+            response.success(theResult);
+        }
+        else if ( request.user.username === pmPhoneNumber )
+        {
+            // Username is Phone Number
+            if ( ( cuFirstName === pmFirstName  ) &&
+                 ( cuLastName === pmLastName    ) )
+            {
+                // First and Last Names match
+                conditionalLog("First and Last names match");
+
+                if ( verificationCode.length > 0 )
+                {
+                    // Have a verification code, attempt to login
+                    conditionalLog("Have a Verification Code");
+
+                    var username    = pmPhoneNumber;
+                    var token       = process.env.USER_SERVICE_TOKEN;
+                    var password    = token + "-" + verificationCode;
+
+                    Parse.User.logIn(username, password,
+                    {
+                        success: function(tempUser)
+                        {
+                            // Was able to login with the passed verification code, so have the correct user
+                            theResult   = {
+                                            action : ( ACTION_USER_ACTIVE | ACTION_USER_LOGGED_IN ),
+                                            description : "Current User Verified",
+                                            sessionToken : tempUser.getSessionToken()
+                                          };
+                            response.success(theResult);
+                        },
+                        error: function(tempUser, error)
+                        {
+                            // The login failed. Check error to see why.
+                            theResult   = {
+                                            action: ( ACTION_USER_VERIFY ),
+                                            description: "Verify User"
+                                          };
+                            response.success(theResult);
+                        }
+                    });
+                }
+                else
+                {
+                    // No Verification Code
+                    theResult   = {
+                                    action : ( ACTION_USER_VERIFY ),
+                                    description: "Verify User"
+                                  };
+                    response.success(theResult);
+                }
+            }
+            else
+            {
+                // First and Last Names don't match
+                theResult   = {
+                                action : ( ACTION_USER_VERIFY ),
+                                description: "Verify User"
+                              };
+                response.success(theResult);
+            }
+        }
+    }
+
+    // Either
+    // No Current User Passed
+    // Or Current User Doesn't Match Passed Info
+    // Check Passed Info
+
+    var phoneQuery          = new Parse.Query(Parse.User);
+    phoneQuery.equalTo("phoneNumber", pmPhoneNumber);
+
+    var emailQuery          = new Parse.Query(Parse.User);
+    emailQuery.equalTo("email", pmEmailAddress);
+
+    var orQuery             = Parse.Query.or(phoneQuery, emailQuery);
+    orQuery.find(
+    {
+        useMasterKey: true,
+        success: function(usersResults)
+        {
+            if ( usersResults.length === 0 )
+            {
+                theResult   = {
+                                action : ( ACTION_USER_CREATE ),
+                                description : "No user found with username as email address or phone number"
+                              };
+                response.success(theResult);
+            }
+            else
+            {
+                // query found one or more users
+                var foundUser       = null;
+                var userVersion     = 0;
+
+                for ( uIdx = 0; uIdx < count(usersResults); uIdx = (uIdx + 1) )
+                {
+                    // Loop through matching users (should only be 0, 1, or 2)
+                    var thisUser    = usersResults[uIdx];
+                    var tuUsername  = thisUser.username;
+                    var tuFirstname = thisUser.get("firstName");
+                    var tuLastName  = thisUser.get("lastName");
+
+                    if ( tuUsername === pmPhoneNumber )
+                    {
+                        // Username matches phone Number
+                        if ( ( tuFirstname === pmFirstName ) &&
+                             ( tuLastName  === pmLastName ) &&
+                             ( verificationCode.length > 0 ) )
+                        {
+                            // First and Last Names match, and have Verification Code
+                            // Assign to foundUser, then verify credentials
+                            foundUser   = thisUser;
+                            userVersion = 2;
+                            break;
+                        }
+                    }
+                    else if ( tuUsername === pmEmailAddress )
+                    {
+                        // Username matches emailAddress
+                        if ( ( tuFirstname === pmFirstName ) &&
+                             ( tuLastName  === pmLastName ) )
+                        {
+                            // First and Last Names match,
+                            // Assign to foundUser, then verify credentials
+                            foundUser   = thisUser;
+                            userVersion = 1;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        // Nothing to do if email and phone don't match
+                        // Although should never get here since the orQuery is for them
+                        console.log("Check This Function, shouldn't get this code");
+                    }
+                }
+
+                if ( foundUser !== null )
+                {
+                    if ( userVersion === 1 )
+                    {
+                        // User found, with email address as username
+                        theResult   = {
+                                        action : ( ACTION_USER_CONVERT | ACTION_USER_VERIFY ),
+                                        description : "Found user matching email address, first, and last names, verify and convert."
+                                      };
+                        response.success(theResult);
+                    }
+                    else if ( userVersion === 2 )
+                    {
+                        // User found, with phone number as username
+                        if ( verificationCode.length > 0 )
+                        {
+                            conditionalLog("Have a Verification Code");
+
+                            var username    = pmPhoneNumber;
+                            var token       = process.env.USER_SERVICE_TOKEN;
+                            var password    = token + "-" + verificationCode;
+
+                            Parse.User.logIn(username, password,
+                            {
+                                success: function(tempUser)
+                                {
+                                    // Was able to login with the passed verification code, so have the correct user
+                                    theResult   = {
+                                                    action : ( ACTION_USER_LOGGED_IN ),
+                                                    description : "Current User Verified",
+                                                    sessionToken : tempUser.getSessionToken()
+                                                  };
+                                    response.success(theResult);
+                                },
+                                error: function(tempUser, tempError)
+                                {
+                                    // The login failed. Check error to see why.
+                                    theResult   = {
+                                                    action: ( ACTION_USER_VERIFY ),
+                                                    description: "Verify User",
+                                                    error: tempError
+                                                  };
+                                    response.success(theResult);
+                                }
+                            });
+                        }
+                        else
+                        {
+                            // No Verification Code
+                            theResult   = {
+                                            action : ( ACTION_USER_VERIFY ),
+                                            description: "Verify User"
+                                          };
+                            response.success(theResult);
+                        }
+                    }
+                }
+                else
+                {
+                    // No User Found,
+                    // Think I can advise to createResult
+                    theResult   = {
+                                    action: ( ACTION_USER_CREATE ),
+                                    description: "No User Found"
+                                  };
+                    response.success(theResult);
+                }
+            }
+        },
+        error: function(usersError)
+        {
+            console.log("Error in orQuery");
+            console.log(usersError);
+
+            response.error(usersError);
         }
     });
 });
@@ -2810,4 +3131,3 @@ Parse.Cloud.define("sendSMS", function(request, response)
         }
     });
 });
-
