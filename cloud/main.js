d0ccba4757d42cb3913ed3b10153a064da42b70b
Rycardo <rycardo.cousteau@yahoo.ca>
2017-03-19 15:35:15 -0400

    moved constants to user.js

--- cloud/main.js
+++ cloud/main.js
@@ -78,18 +78,6 @@ var twilioURL               = process.env.TWILIO_URL            || "127.0.0.1";
 var twilioMount             = process.env.TWILIO_MOUNT          || "/";
 var twilioSendingNumber     = process.env.TWILIO_PHONE_NUMBER;
 
-///////////////////////////////////////
-// CONSTANTS
-//////////////////////////////////////
-
-// User Actions - for verifying a User
-const ACTION_USER_ERROR         = 0;
-const ACTION_USER_CREATE        = 1;
-const ACTION_USER_LOGIN         = 2;
-const ACTION_USER_CONVERT       = 4;
-const ACTION_USER_VERIFY        = 8;
-const ACTION_USER_ACTIVE        = 16;
-const ACTION_USER_LOGGED_IN     = 32;
 
 //////////////////////////////////////
 //
